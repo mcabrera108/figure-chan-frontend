02e3e74b47aa455a16d55eb2d59cb57ff01fefd5
mcabrera108 <martin.cabrera103@gmail.com>
2025-02-05 01:39:55 -0600

    Added Construction Banner Image

--- src/pages/ConstructionPage.jsx
+++ src/pages/ConstructionPage.jsx
@@ -1,11 +1,17 @@
 import homestyle from "../style_modules/home.module.scss";
 import errorstyle from "../style_modules/errorpage.module.scss";
+import figureChanConstruction from "../assets/figure-chan-construction-banner.png";
 function ConstructionPage() {
   return (
     <div className={homestyle.mainContainer}>
       <div className={homestyle.homeContainer}>
         <div className={homestyle.homeWrapper}>
           <div className={errorstyle.errorContainer}>
+            <img
+              src={figureChanConstruction}
+              alt="Figure-Chan Construction"
+              className={errorstyle.figureChanBanner}
+            />
             <h1>This page is currently under construction</h1>
           </div>
         </div>
