ba9a428a059c94c7dbace9b7315ace259f7e3495
mcabrera108 <martin.cabrera103@gmail.com>
2025-02-05 10:55:57 -0600

    Reworked API Endpoint

--- src/services/APIUserMethods.js
+++ src/services/APIUserMethods.js
@@ -9,9 +9,10 @@ async function APIRegisterUser(formData) {
       password: formData.password,
     }),
   };
+  // "https://figure-chan-user-api-63a090ba3519.herokuapp.com/register"
   try {
     const response = await fetch(
-      "http://localhost:3000/register",
+      "https://figure-chan-user-api-63a090ba3519.herokuapp.com/register",
       requestOptions
     );
     const data = await response.json();
@@ -32,7 +33,10 @@ async function APILoginUser(formData) {
     }),
   };
   try {
-    const response = await fetch("http://localhost:3000/login", requestOptions);
+    const response = await fetch(
+      "https://figure-chan-user-api-63a090ba3519.herokuapp.com/login",
+      requestOptions
+    );
     const data = await response.json();
     return data;
   } catch (error) {
